f64a9e0e7a14c97dc6838f8a900454f9419e9e74
Darmaan <aldarmaan@gmail.com>
2023-02-05 20:58:29 +0000

    add to existing repo

--- hello.py
+++ hello.py
@@ -7,3 +7,6 @@ c = a + b
 print("Python says: Hello, World!")
 print("%s + %s = %s" % (a,b,c))
 
+userList = ["User1", "User2", "User3"];
+for user in userList:
+    print(user)
--- hello.sh
+++ hello.sh
@@ -6,4 +6,9 @@
               
               echo "Bash says: Hello, World!"
               echo "$a + $b = $c"
-
+	      
+              declare -a myList=("User1" "User2" "User3")
+              for user in "${myList[@]}"
+              do
+                      echo "$user"
+              done
